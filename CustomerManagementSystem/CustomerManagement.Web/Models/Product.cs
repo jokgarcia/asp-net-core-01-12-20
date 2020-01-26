b33b62150a2fccb89a6f53342c2dc1e04ce45363
Administrator <Administrator@130-14-911>
2020-01-26 14:17:57 +0800

    added product

---
+++ CustomerManagementSystem/CustomerManagement.Web/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace CustomerManagement.Web.Models
+{
+    public class Product
+    {
+        //Attributes and Data types
+        public int Id { get; set; }
+        public string PRoductName { get; set; }
+        public string Description { get; set; }
+        public string SupplierName { get; set; }
+    }
+}
