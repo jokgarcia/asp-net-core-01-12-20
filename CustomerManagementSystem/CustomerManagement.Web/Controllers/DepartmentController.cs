03c4852168dbf6317d9d227d7fa6c3df17bb3342
mcjavier <mcjavier@130-18-1280>
2020-01-26 14:00:36 +0800

    added department

---
+++ CustomerManagementSystem/CustomerManagement.Web/Controllers/DepartmentController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerManagement.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerManagement.Web.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private IRepositoryLayer repositoryLayer;
+
+        public DepartmentController(IRepositoryLayer _repositoryLayer)
+        {
+            repositoryLayer = _repositoryLayer;
+        }
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+    }
+}
---
+++ CustomerManagementSystem/CustomerManagement.Web/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.Web.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string DepartmentName { get; set; }
+        public string Manager { get; set; }
+    }
+}
